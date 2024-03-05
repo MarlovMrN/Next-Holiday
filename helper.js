bf73df35fc0212ed00bf4f3a4fb846c75a3fc06b
Marlon Griebeler da Hora <marlongriebeler2@gmail.com>
2024-03-04 22:31:06 -0300

    re-scoping the project to show only brazil and national holidays

---
+++ helper.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import pkg from "geoip-lite";
+const { lookup } = pkg;
+
+import countryData from "./country-data.js";
+
+const apiKey = process.env.NEXTHOLIDAY_APIKEY;
+const apiURL = "https://calendarific.com/api/v2";
+const apiEndpoint = "/holidays";
+
+export async function fetchHolidays(countryAcronym) {
+  const options = {
+    params: {
+      api_key: apiKey,
+      country: countryAcronym,
+      year: new Date().getFullYear(),
+    },
+  };
+  try {
+    const result = await axios.get(apiURL + apiEndpoint, options);
+    return result.data.response.holidays;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export function futureHolidaysOnly(holidays) {
+  const today = new Date();
+  const currentMonth = today.getMonth() + 1;
+  const currentDay = today.getDay() + 1;
+  const result = holidays.filter((entry) => {
+    if (
+      entry.date.datetime.month > currentMonth &&
+      entry.primary_type.toLowerCase().includes("holiday")
+    ) {
+      return true;
+    } else if (
+      entry.date.datetime.month == currentMonth &&
+      entry.date.datetime.day > currentDay &&
+      entry.primary_type.toLowerCase().includes("holiday")
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return result;
+}
+
+export function getCountryData(request) {
+  const countryFromEntry = getCountryEntry(request.query.country);
+  const countryFromIP = getCountryAcronymByRequest(request);
+  return countryFromEntry || countryFromIP || { acronym: "BR" };
+}
+
+export function getCountryEntry(countryAcronym) {
+  const country = countryData.find((element) => {
+    element.acronym === countryAcronym;
+  });
+  return country;
+}
+
+export function getCountryAcronymByRequest(req) {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const lookupIP = lookup(ip);
+  const countryAcronym = lookupIP ? lookupIP.country : "";
+  return countryAcronym;
+}
--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-import axios from "axios";
 import express from "express";
-import pkg from "geoip-lite";
-import countryData from "./country-data.js";
-const { lookup } = pkg;
+import { fetchHolidays, futureHolidaysOnly, getCountryData } from "./helper.js";
 
 const app = express();
 const port = 3000;
-
-const apiKey = process.env.NEXTHOLIDAY_APIKEY;
-const apiURL = "https://calendarific.com/api/v2";
-const apiEndpoint = "/holidays";
 
 app.get("/", async (req, res) => {
   const country = getCountryData(req);
@@ -35,66 +28,6 @@ app.get("/500", (req, res) => {
   //TO-DO create 500 page
   res.send("Something went wrong");
 });
-
-async function fetchHolidays(countryAcronym) {
-  const options = {
-    params: {
-      api_key: apiKey,
-      country: countryAcronym,
-      year: new Date().getFullYear(),
-    },
-  };
-  try {
-    const result = await axios.get(apiURL + apiEndpoint, options);
-    return result.data.response.holidays;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function futureHolidaysOnly(holidays) {
-  const today = new Date();
-  const currentMonth = today.getMonth() + 1;
-  const currentDay = today.getDay() + 1;
-  const result = holidays.filter((entry) => {
-    if (
-      entry.date.datetime.month > currentMonth &&
-      entry.primary_type.toLowerCase().includes("holiday")
-    ) {
-      return true;
-    } else if (
-      entry.date.datetime.month == currentMonth &&
-      entry.date.datetime.day > currentDay &&
-      entry.primary_type.toLowerCase().includes("holiday")
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  return result;
-}
-
-function getCountryData(request) {
-  const countryFromEntry = getCountryEntry(request.query.country);
-  const countryFromIP = getCountryAcronymByRequest(request);
-  return countryFromEntry || countryFromIP || { acronym: "BR" };
-}
-
-function getCountryEntry(countryAcronym) {
-  const country = countryData.find((element) => {
-    element.acronym === countryAcronym;
-  });
-  return country;
-}
-
-function getCountryAcronymByRequest(req) {
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  const lookupIP = lookup(ip);
-  const countryAcronym = lookupIP ? lookupIP.country : "";
-  return countryAcronym;
-}
 
 app.listen(port, () => {
   console.log("Server Listening on port", port);
